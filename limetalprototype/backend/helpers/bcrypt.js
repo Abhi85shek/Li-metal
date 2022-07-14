8984d92ef75378e53017d3e19b04bf2acc727e32
Abhi85shek <abhishek.kumar@consulero.com>
2022-07-14 19:55:38 +0000

    Chnages

--- limetalprototype/backend/helpers/bcrypt.js
+++ limetalprototype/backend/helpers/bcrypt.js
@@ -3,6 +3,7 @@ require('dotenv');
 const hashing = async (password)=>{
     const salt = await bcrypt.genSaltSync(10);
     const hash = await bcrypt.hashSync(password, salt);
+    console.log(hash);
     return hash;
 };
 module.exports = hashing;
--- limetalprototype/backend/routes/users.js
+++ limetalprototype/backend/routes/users.js
@@ -3,9 +3,45 @@ var router = express.Router();
 const jwt =require('jsonwebtoken');
 const checkAuth = require('../middleware/check-auth');
 const hashing  = require('../helpers/bcrypt');
+const bcrypt = require('bcrypt');
+
+hashing("admin123");
+router.post("/login",async (req,res)=>{
+  const email = req.body.email;
+  const password = req.body.password;
+  db.query(`SELECT * FROM limetalusers WHERE email=?`,[email],async (err,result)=>{
+      if(err)
+      {
+          res.status(404).send({
+              message:"Query Not Working"
+          });
+      }
+      if(result.length!=0)
+      {
+              const Passwordmatch = await bcrypt.compare(password,result[0].password);
+              if(Passwordmatch)
+              {
+
+                  // creating a JWT Token and Send Email as a Payload
+
+                  const token = jwt.sign({
+                          email:result[0].email
+                  },process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'});
+                  res.status(200).json({message:"Login Successful",userId:result[0].userid,userEmail:result[0].email,token,userType:result[0].userType,userValid:true,userName:result[0].fullName});
+              }
+              else
+              {
+                  res.json({message:"Please Check Your Password",userValid:false});
+              }      
+      }
+      else
+      {
+                  res.json({message:"No User Found",userValid:false});
+      } 
+  })
+
+});
+
+
 router.use(checkAuth);
-
-console.log(hashing("admin"));
-
-
 module.exports = router;
