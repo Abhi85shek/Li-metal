f591df33ca48be2900e7443cb9132e2bd5d34c6f
Abhi85shek <abhishek.kumar@consulero.com>
2022-07-21 20:45:04 +0000

    QuickBooks API

---
+++ limetalprototype/backend/config/config.js
@@ -0,0 +1,11 @@
+
+const config = module.exports ={
+
+        qb:{
+            quickBookLocalID : 1,
+            BASE_URL:'https://sandbox-quickbooks.api.intuit.com',
+            COMPANY_NUMBER:'4620816365234724280'
+
+        }
+
+};
