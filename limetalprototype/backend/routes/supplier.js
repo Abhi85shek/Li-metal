77ef331c0b7c2da09a040053f104eafa1f3aed89
Abhi85shek <abhishek.kumar@consulero.com>
2022-07-22 18:50:56 +0000

    Changes

--- limetalprototype/backend/routes/supplier.js
+++ limetalprototype/backend/routes/supplier.js
@@ -34,7 +34,7 @@ const db = require('../helpers/db');
         const {company} = req.body.supplierDetails;
         const {streetAddress} = req.body.supplierDetails;
         const {city} = req.body.supplierDetails;
-        const {proviance} = req.body.supplierDetails;
+        const {Province} = req.body.supplierDetails;
         const {country} = req.body.supplierDetails;
         const {postalCode} = req.body.supplierDetails;
         const {taxSlip} = req.body.supplierDetails;
@@ -44,7 +44,7 @@ const db = require('../helpers/db');
         const {supplierNumber} = req.body.supplierDetails;
         const {currency} = req.body.supplierDetails;
 
-        db.query("UPDATE suppliertable SET supplier=?,company=?,streetAddress=?,city=?,Province=?,Country=?,postalCode=?,taxSlip=?,phone=?,email=?,openBalance=?,supplierNumber=?,currency=? WHERE id=?",[supplierName,company,streetAddress,city,proviance,country,postalCode,taxSlip,phone,email,openBalance,supplierNumber,currency,req.params.id],(err,result)=>{
+        db.query("UPDATE suppliertable SET supplier=?,company=?,streetAddress=?,city=?,Province=?,Country=?,postalCode=?,taxSlip=?,phone=?,email=?,openBalance=?,supplierNumber=?,currency=? WHERE id=?",[supplierName,company,streetAddress,city,Province,country,postalCode,taxSlip,phone,email,openBalance,supplierNumber,currency,req.params.id],(err,result)=>{
 
             if(err)
             {
