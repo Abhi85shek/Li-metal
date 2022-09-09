a7ba8f1147ca70f6c9f9edfe38521712416a627c
Abhi85shek <abhishek.kumar@consulero.com>
2022-09-09 13:43:09 +0000

    Changes in Get Supplier API

--- limetalprototype/backend/routes/supplier.js
+++ limetalprototype/backend/routes/supplier.js
@@ -84,7 +84,7 @@ router.get('/allSuppliers/:curr_page/:curr_count',async (req,res)=>{
 // Get SupplierName and SupplierNumber
     router.get('/getSuppliers',async (req,res)=>{
         try {
-            db.query('SELECT name,supplierNumber,qbId,poCount FROM vendors',(err,result)=>{
+            db.query(`SELECT name,supplierNumber,qbId,poCount FROM vendors WHERE supplierNumber !=' '`,(err,result)=>{
                 if(err)
                     {
                         throw err;
