f73e5ddfdb400b170e2de1aa04406207d5a37c4e
Harshit <harshit.singh@consulero.com>
2022-10-14 13:37:35 -0400

    qbadmincontrols

--- limetalprototype/src/component/ViewAllSuperAdminOrders.jsx
+++ limetalprototype/src/component/ViewAllSuperAdminOrders.jsx
@@ -65,7 +65,7 @@ const ViewAllSuperAdminOrders = () => {
             },data:orderObj,refreshToken:quickbooksCredentials,poId:po.id
           })
           console.log(res)
-          if(res.status==201)
+          if(res.status==200)
           {
             setTimeout(()=>{
                 toast.success('Order Successfullly stored', {
